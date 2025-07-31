f1e6007726c24bd1ed42924138a381d406568c36
dakeypunchar <0dakeypunchar0@gmail.com>
2025-07-31 23:31:10 +0530

    Added Escape to exit on Main Menu

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,5 +1,9 @@
 extends Control
 
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("escape"):
+		get_tree().quit();
+
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 	Global.game_state = Global.GameState.GAMEPLAY;
