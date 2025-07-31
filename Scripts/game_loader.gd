1a467754acb8844627a0790ef94ec216196dcb17
pkhead <chromosoze@gmail.com>
2025-07-31 14:42:16 -0500

    add hazard custom data to tileset

--- Scripts/game_loader.gd
+++ Scripts/game_loader.gd
@@ -4,12 +4,10 @@ const collectibles: Dictionary[Vector2i, Resource] = {
 	Vector2i(0, 0): preload("res://Objects/stamina_collectible.tscn")
 }
 
-var deadly_tiles: Array[Vector2i] = [Vector2i(0, 1), Vector2i(2, 0)]
-
 func _ready() -> void:
 	var collectibles_tile_map: TileMapLayer = $CollectiblesTileMap
 	var tile_map: TileMapLayer = $TileMap
-	var deadly_area: Area2D = $DeadlyTiles
+	var deadly_area: Area2D = $DeadlyTiles	
 	
 	# read the collectibles tilemap to instantiate collectible instances
 	var ct_bounds := collectibles_tile_map.get_used_rect()	
@@ -33,10 +31,10 @@ func _ready() -> void:
 	for y in range(main_bounds.position.y, main_bounds.end.y):
 		for x in range(main_bounds.position.x, main_bounds.end.x):
 			var tile_pos := Vector2i(x, y)
-			var tile_uv := tile_map.get_cell_atlas_coords(tile_pos)
+			var tile_data := tile_map.get_cell_tile_data(tile_pos)
 			
 			# create collider at this cell
-			if deadly_tiles.has(tile_uv):
+			if tile_data != null and tile_data.get_custom_data("Hazard"):
 				var collider := CollisionShape2D.new()
 				var colshape := RectangleShape2D.new()
 				colshape.size = tile_size - Vector2i(4, 4)
