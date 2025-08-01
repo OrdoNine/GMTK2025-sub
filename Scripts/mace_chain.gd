1db40d3f821a8dab18ca2810275b14bc2162861c
OrdoNine <124820586+OrdoNine@users.noreply.github.com>
2025-08-01 20:54:26 +0300

    completed mace behavior

--- Scripts/mace_chain.gd
+++ Scripts/mace_chain.gd
@@ -1,17 +1,21 @@
 extends Line2D
 class_name MaceChain
 
-@export var mace := Node2D #the object that is chained to this
+@export var mace : Node2D #the object that is chained to this.
+@export var tilemap : TileMapLayer  #the tilemap as a reference for path pixelization. doesnt pixelize if null.
+@export var target : Node2D
 
 signal knocked_back()
 signal completed_loop()
 
-var mace_speed := 5.0 
+var mace_speed := 2.0 
 var mace_accel := 0.5 
 var mace_vel := Vector2()
 var mace_range := 100.0
 
 var reversed := false #whether the mace will move backwards 
+var advance := false
+var loop_dist := 0.0
 var knockback := 0.0 #how much reversed will last.
 
 var curve := Curve2D.new()
@@ -22,35 +26,9 @@ func _ready() -> void:
 	changePattern(mace_range)
 	print("something")
 
-
-func changePattern(range : float = 1, rot_deg : float = 0, idx : int = -1):
-	#negative index picks randomly from available moves
-	if Global.maceAttackPatterns: 
-		if idx < 0:
-			idx = randi_range(0, Global.maceAttackPatterns.keys().size() - 1)
-		var next_move = Global.maceAttackPatterns.keys()[idx]
-		var pattern = Global.maceAttackPatterns[next_move]["points"]
-		curve.clear_points()
-		
-		var rot_rad = deg_to_rad(rot_deg)
-		for i in range(pattern.size()):
-			curve.add_point(
-							mace.global_position + Vector2(pattern[i][0][0], pattern[i][0][1]).rotated(rot_rad) * range, 
-							Vector2(pattern[i][1][0], pattern[i][1][1]).rotated(rot_rad) * range,
-							Vector2(pattern[i][2][0], pattern[i][2][1]).rotated(rot_rad) * range
-							)
-		curve.tessellate()
-		points = curve.get_baked_points()
-
-func reverse(kb : float):
-	if kb > 0:
-		reversed = true
-		knockback = kb
-		emit_signal("knocked_back")
-
 func _process(delta: float) -> void:
 	if mace:
-		if completion >= curve.get_baked_length():
+		if completion >= loop_dist:
 			completion = 0
 			loops += 1
 			emit_signal("completed_loop")
@@ -62,4 +40,45 @@ func _process(delta: float) -> void:
 		mace.global_position = curve.sample_baked(completion)
 
 func _on_completed_loop() -> void:
-	changePattern(mace_range, [0,45,90,-45,-90].pick_random())
+	var new_rot = [0,45,90,-45,-90].pick_random()
+	if target:
+		advance = mace.global_position.distance_squared_to(target.global_position) > mace_range * 200
+		print(mace.global_position.distance_squared_to(target.global_position))
+		new_rot = rad_to_deg(mace.global_position.angle_to_point(target.global_position))
+	changePattern(mace_range, new_rot)
+
+func changePattern(range : float = 1, rot_deg : float = 0, idx : int = -1):
+	#negative index picks randomly from available moves
+	
+	if Global.maceAttackPatterns: 
+		if idx < 0:
+			idx = randi_range(0, Global.maceAttackPatterns.keys().size() - 1)
+		var next_move = Global.maceAttackPatterns.keys()[idx]
+		var pattern = Global.maceAttackPatterns[next_move]["points"]
+		
+		curve.clear_points()
+		
+		var rot_rad = deg_to_rad(rot_deg - Global.maceAttackPatterns[next_move]["rotation"])
+		var start = mace.global_position
+		if tilemap:
+			start = tilemap.map_to_local( tilemap.local_to_map(mace.global_position) )
+		
+		for i in range(pattern.size()):
+			curve.add_point(
+							start + Vector2(pattern[i][0][0], pattern[i][0][1]).rotated(rot_rad) * range, 
+							Vector2(pattern[i][1][0], pattern[i][1][1]).rotated(rot_rad) * range,
+							Vector2(pattern[i][2][0], pattern[i][2][1]).rotated(rot_rad) * range
+							)
+		
+		curve.tessellate()
+		points = curve.get_baked_points()
+		
+		loop_dist = curve.get_baked_length()
+		if advance:
+			loop_dist *= Global.maceAttackPatterns[next_move]["advance"]
+
+func reverse(kb : float):
+	if kb > 0:
+		reversed = true
+		knockback = kb
+		emit_signal("knocked_back")
