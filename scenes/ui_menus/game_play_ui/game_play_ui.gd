b5f2de78e34e0cbcfbeeb1e9c28a3a3b1cb0f0f3
dakeypunchar <0dakeypunchar0@gmail.com>
2025-08-03 17:13:09 +0530

    UI change, part I

--- scenes/ui_menus/game_play_ui/game_play_ui.gd
+++ scenes/ui_menus/game_play_ui/game_play_ui.gd
@@ -4,6 +4,9 @@ var stamina_points: int = 0;
 var time_remaining: float = 0.0;
 var round_number: int = 0
 
+const can_use_color := Color.CORNSILK;
+const cannot_use_color := Color.DARK_SLATE_GRAY;
+
 func _ready() -> void:
 	Global.ui_update.connect(_on_ui_update);
 
@@ -12,6 +15,20 @@ func _on_ui_update(new_time_remaining: float, new_round_number: int):
 	round_number = new_round_number;
 
 func _process(_delta: float) -> void:
-	var status_text: Label = get_node("Status")
-	status_text.text = "Round: %s\nSlime: %s\nTime remaining: %.2f" % \
-		[round_number, stamina_points, time_remaining]
+	%RoundNumber.text = "Round: " + str(round_number)
+	%Slimes.text = "x" + str(stamina_points)
+	
+	if stamina_points >= 5:
+		%CanCreateSlimeBomb.modulate = can_use_color;
+	else:
+		%CanCreateSlimeBomb.modulate = cannot_use_color;
+	if stamina_points >= 6:
+		%CanCreateBooster.modulate = can_use_color;
+		%CanCreateSpring.modulate = can_use_color;
+	else:
+		%CanCreateSpring.modulate = cannot_use_color;
+		%CanCreateBooster.modulate = cannot_use_color;
+	if stamina_points >= 8:
+		%CanCreateMidAirBridge.modulate = can_use_color;
+	else:
+		%CanCreateMidAirBridge.modulate = cannot_use_color;
