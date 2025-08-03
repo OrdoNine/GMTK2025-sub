e183674167b4117fac6bdc69ab0ee144dcd66570
pkhead <chromosoze@gmail.com>
2025-08-03 12:43:07 -0500

    fix the god damn credits

--- scenes/ui_menus/credits/credits.gd
+++ scenes/ui_menus/credits/credits.gd
@@ -5,10 +5,18 @@ func _ready() -> void:
 
 func _on_gamemode_changed(from_state: Global.GameState, to_state: Global.GameState):
 	self.visible = to_state == Global.GameState.CREDITS;
+	
+	# WHY DO YOU HAVE TO PAUSE THE GAME SO THAT THE BACK BUTTON WORKS 
+	if to_state == Global.GameState.CREDITS:
+		get_tree().paused = true
+	else:
+		get_tree().paused = false
 
 func _on_back_button_pressed() -> void:
+	Global.game_state = Global.GameState.MAIN_MENU
+	print("pressed")
 	pass
 
 
 func _on_back_button_released() -> void:
-	get_tree().quit();
+	pass
