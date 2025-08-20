5a6ec75fc7e0195cc56d1a71c8ba772d6398d7e7
dakeypunchar <0dakeypunchar0@gmail.com>
2025-08-20 19:46:22 +0530

    Made macey open mouth and flip horizontally

--- objects/mace/mace.gd
+++ objects/mace/mace.gd
@@ -4,3 +4,13 @@ class_name Mace
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is Player:
 		body.kill()
+
+const OPEN_MOUTH_ZONE_DISTANCE : float = 100
+const OPEN_MOUTH_ZONE_DISTANCE_SQUARED : float = OPEN_MOUTH_ZONE_DISTANCE * OPEN_MOUTH_ZONE_DISTANCE
+
+func _process(_dt: float) -> void:
+	var difference : Vector2 = position - %Player.position
+	$AnimatedSprite2D.flip_h = difference.x > 0
+	$AnimatedSprite2D.animation = "open_mouth" if\
+				difference.length_squared() < OPEN_MOUTH_ZONE_DISTANCE_SQUARED\
+				else "close_mouth"
