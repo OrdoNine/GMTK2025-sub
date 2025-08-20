3f1cd08348d613fc9b27d4f5dd475d7f0cc4e145
pkhead <chromosoze@gmail.com>
2025-08-20 12:01:36 -0500

    round end timer and input adjustments - timer ends one second earlier - only pauses when pause key is pressed

--- scenes/processes/game/ui.gd
+++ scenes/processes/game/ui.gd
@@ -3,7 +3,7 @@ class_name UI
 
 # Variables for Warmup Menu
 var countdown_active := true
-var countdown_timer := PollTimer.new(3)
+var countdown_timer := PollTimer.new(2)
 
 func _on_resume_button_pressed() -> void:
 	%PauseUI.visible = false
@@ -28,17 +28,13 @@ func _on_game_round_ended() -> void:
 
 
 func _input(event: InputEvent) -> void:
-	if not (event is InputEventKey and not event.is_echo() and event.is_pressed()):
-		return
-	
-	if not %WarmupMenu.visible or not countdown_active:
-		return
-	
-	# Some key has pressed if we reached here.
-	# So switch to non-countdown active menu for the player to rest.
-	%CountdownMode.visible = false
-	%IndefiniteMode.visible = true
-	countdown_active = false
+	# pause the countdown timer when pause is pressed
+	if event is InputEventKey:
+		if event.pressed and event.is_action("game_pause"):
+			if %WarmupMenu.visible and countdown_active:
+				%CountdownMode.visible = false
+				%IndefiniteMode.visible = true
+				countdown_active = false
 
 
 func _process(dt: float) -> void:
